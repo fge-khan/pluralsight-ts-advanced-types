fe1c1b7f1696e975b0040e1de937a8d30b80f7e0
franck <franckengoulevent@gmail.com>
2022-06-08 00:49:25 +0200

    Using function overloading to discriminate type

--- lessons/4-Making-Your-Types-More-Flexible-with-Conditional-Types/lesson.1.begin.ts
+++ lessons/4-Making-Your-Types-More-Flexible-with-Conditional-Types/lesson.1.begin.ts
@@ -5,39 +5,53 @@ import {
   LayerType,
   Size,
   TextMeta,
-  ImageMeta
-} from "./types";
-import { render } from "./render";
+  ImageMeta,
+} from './types';
+import { render } from './render';
+import { Layer } from '../9-Modelling-Idiomatic-Data-Sources-with-Iterators/types';
 
 const projectSize: Size = {
   width: 512,
-  height: 250
+  height: 250,
 };
 
 const textLayer: TextLayer = {
   type: LayerType.Text,
   maxWidth: 1000,
   position: { x: 128, y: 208 },
-  color: "#e8166d",
-  id: "10",
+  color: '#e8166d',
+  id: '10',
   rotation: 0,
-  text: "Advanced TypeScript",
-  fontSize: "20px"
+  text: 'Advanced TypeScript',
+  fontSize: '20px',
 };
 
 const imageLayer: ImageLayer = {
   type: LayerType.Image,
 
   position: { x: 0, y: 0 },
-  id: "20",
+  id: '20',
   rotation: 0,
-  src: "ps-dark.png",
-  maxBounds: { width: projectSize.width }
+  src: 'ps-dark.png',
+  maxBounds: { width: projectSize.width },
 };
+
+//using function overloading to type f setMeta.
+function setMeta(layer: ImageLayer, meta: ImageMeta): void;
+function setMeta(Layer: TextLayer, meta: TextMeta): void;
+
+function setMeta(layer: ImageLayer | TextLayer, meta: ImageMeta | TextMeta) {
+  layer.meta = meta;
+}
+
+setMeta(imageLayer, {
+  origin: 'orign',
+  format: 'png',
+});
 
 const project: Project = {
   layers: [imageLayer, textLayer],
-  size: projectSize
+  size: projectSize,
 };
 
 render(project);
