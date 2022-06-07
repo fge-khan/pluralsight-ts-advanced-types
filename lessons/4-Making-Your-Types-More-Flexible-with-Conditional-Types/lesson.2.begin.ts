f1e982e106d9bfb7d3c03d36c60e6643a2de28e6
franck <franckengoulevent@gmail.com>
2022-06-08 00:55:27 +0200

    Using generics to make one type dependent of another

--- lessons/4-Making-Your-Types-More-Flexible-with-Conditional-Types/lesson.2.begin.ts
+++ lessons/4-Making-Your-Types-More-Flexible-with-Conditional-Types/lesson.2.begin.ts
@@ -5,58 +5,56 @@ import {
   LayerType,
   Size,
   TextMeta,
-  ImageMeta
-} from "./types";
-import { render } from "./render";
+  ImageMeta,
+} from './types';
+import { render } from './render';
 
 const projectSize: Size = {
   width: 512,
-  height: 250
+  height: 250,
 };
 
 const textLayer: TextLayer = {
   type: LayerType.Text,
   maxWidth: 1000,
   position: { x: 128, y: 208 },
-  color: "#e8166d",
-  id: "10",
+  color: '#e8166d',
+  id: '10',
   rotation: 0,
-  text: "Advanced TypeScript",
-  fontSize: "20px"
+  text: 'Advanced TypeScript',
+  fontSize: '20px',
 };
 
 const imageLayer: ImageLayer = {
   type: LayerType.Image,
 
   position: { x: 0, y: 0 },
-  id: "20",
+  id: '20',
   rotation: 0,
-  src: "ps-dark.png",
-  maxBounds: { width: projectSize.width }
+  src: 'ps-dark.png',
+  maxBounds: { width: projectSize.width },
 };
 
-function setMeta(layer: TextLayer, meta: TextMeta): void;
-function setMeta(layer: ImageLayer, meta: ImageMeta): void;
-function setMeta(
-  layer: ImageLayer | TextLayer,
-  meta: ImageMeta | TextMeta
-): void {
-  layer.meta = meta;
+function setMeta<T extends ImageLayer | TextLayer>(
+  layer: T,
+  meta: T extends ImageLayer ? ImageMeta : TextMeta
+) {
+  layer.meta;
 }
 
 setMeta(imageLayer, {
-  format: "png",
-  origin: "Download"
+  format: 'png',
+  origin: 'Download',
 });
 
 setMeta(textLayer, {
-  fontFoundry: "OS stock",
-  licenseExpiration: new Date(2020, 1, 1)
+  fontFoundry: 'OS stock',
+  licenseExpiration: new Date(2020, 1, 1),
 });
 
 const project: Project = {
   layers: [imageLayer, textLayer],
-  size: projectSize
+  size: projectSize,
 };
 
 render(project);
